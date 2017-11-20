65e3b96f9a7aeeb7ef5c6351907b4eefc6498f1a
Marcin Panek <marcin.panek06@gmail.com>
2017-11-20 21:07:26 +0100

    SPI 8/16bit mode switch

--- src/main.c
+++ src/main.c
@@ -6,6 +6,11 @@ volatile uint32_t tmp = 0;
 #define GYRO_CS_LOW()      GPIOC->ODR &= ~(GPIO_ODR_OD1)
 #define GYRO_CS_HIGH()     GPIOC->ODR |=  (GPIO_ODR_OD1)
 
+#define L3GD20_CTRL_REG1_ADDR         0x20  /* Control register 1 */
+#define L3GD20_CTRL_REG2_ADDR         0x21  /* Control register 2 */
+#define L3GD20_CTRL_REG3_ADDR         0x22  /* Control register 3 */
+#define L3GD20_CTRL_REG4_ADDR         0x23  /* Control register 4 */
+#define L3GD20_CTRL_REG5_ADDR         0x24  /* Control register 5 */
 
 void SysClockConfiguration();
 void SysTickConfiguration();
@@ -20,13 +25,12 @@ void LCD_Config(void);
 void LCD_Delay(uint8_t del);
 void LCD_GPIO_Config(void);
 
-void SPI_Tx(uint8_t data);
-uint16_t SPI_Rx(void);
-uint8_t SPI_TxRx(uint8_t data);
+uint32_t SPI_TxRx(uint16_t data, uint8_t RW);
 
 void Gyro_Init();
 void Gyro_GPIO_Config();
-void GYRO_IO_Write(uint8_t* pBuffer, uint8_t WriteAddr);
+void GYRO_IO_Write(uint16_t pBuffer, uint16_t WriteAddr);
+void Gyro_ReadData();
 
 int main(void)
 {
@@ -56,8 +60,11 @@ void SysTick_Handler()
 	if(0 == tmp%10)
 	{
 		GPIOG->ODR ^= GPIO_ODR_ODR_13;
+
+		Gyro_ReadData();
 	}
 	GPIOG->ODR ^= GPIO_ODR_ODR_14;
+
 }
 
 void SysClockConfiguration()
@@ -122,11 +129,9 @@ void GPIOConfiguration()
 
 	GPIOE->AFR[1]  |=  GPIO_AFRH_AFRH1_0 | GPIO_AFRH_AFRH3_0 | GPIO_AFRH_AFRH5_0 | GPIO_AFRH_AFRH6_0 ;
 
-
-
 	//SPI
 	// PF7, PF8, PF9
-	GPIOF->MODER   |= GPIO_MODER_MODE7_1 | GPIO_MODER_MODE8_1 | GPIO_MODER_MODE9_1 ;
+	GPIOF->MODER   |= GPIO_MODER_MODE7_1  | GPIO_MODER_MODE9_1 ;
 	//hight speed
 	GPIOF->OSPEEDR |= GPIO_OSPEEDR_OSPEED7_0  | GPIO_OSPEEDR_OSPEED8_0 | GPIO_OSPEEDR_OSPEED9_0;
 
@@ -165,31 +170,20 @@ void TIMConfiguration()
 void SPIConfiguration()
 {
 	SPI5->CR1 = SPI_CR1_SPE | SPI_CR1_BR_0 | SPI_CR1_BR_1 | SPI_CR1_SSM | SPI_CR1_SSI | SPI_CR1_MSTR;
+	SPI5->CR1 |= 1<<1 | 1<<0;
 }
 
-void SPI_Tx(uint8_t data)
+// RW: 1=R, 0=W
+uint32_t SPI_TxRx(uint16_t data, uint8_t RW)
 {
+	data |= (uint16_t)RW<<15;
+
+	while( !(SPI5->SR & (SPI_SR_TXE)) );
 	SPI5->DR = data;
-	while( !(SPI5->SR & (SPI_SR_TXE) ));
-	while( (SPI5->SR & (SPI_SR_BSY) ));
+	while( !(SPI5->SR & (SPI_SR_RXNE)) );
+	uint32_t xspk = SPI5->DR;
+	return xspk;
 }
-
-uint16_t SPI_Rx()
-{
-	uint16_t data = 0;
-
-	while( !(SPI5->SR & SPI_SR_RXNE) );
-	data = SPI5->DR;
-	return data;
-}
-
-
-uint8_t SPI_TxRx(uint8_t data)
-{
-	SPI_Tx(data);
-	return SPI_Rx();
-}
-
 
 void ili9341_Init(void)
 {
@@ -197,122 +191,122 @@ void ili9341_Init(void)
   LCD_IO_Init();
 
   /* Configure LCD */
-  ili9341_WriteReg(0xCA);
-  ili9341_WriteData(0xC3);
-  ili9341_WriteData(0x08);
-  ili9341_WriteData(0x50);
-  ili9341_WriteReg(LCD_POWERB);
-  ili9341_WriteData(0x00);
-  ili9341_WriteData(0xC1);
-  ili9341_WriteData(0x30);
-  ili9341_WriteReg(LCD_POWER_SEQ);
-  ili9341_WriteData(0x64);
-  ili9341_WriteData(0x03);
-  ili9341_WriteData(0x12);
-  ili9341_WriteData(0x81);
-  ili9341_WriteReg(LCD_DTCA);
-  ili9341_WriteData(0x85);
-  ili9341_WriteData(0x00);
-  ili9341_WriteData(0x78);
-  ili9341_WriteReg(LCD_POWERA);
-  ili9341_WriteData(0x39);
-  ili9341_WriteData(0x2C);
-  ili9341_WriteData(0x00);
-  ili9341_WriteData(0x34);
-  ili9341_WriteData(0x02);
-  ili9341_WriteReg(LCD_PRC);
-  ili9341_WriteData(0x20);
-  ili9341_WriteReg(LCD_DTCB);
-  ili9341_WriteData(0x00);
-  ili9341_WriteData(0x00);
-  ili9341_WriteReg(LCD_FRMCTR1);
-  ili9341_WriteData(0x00);
-  ili9341_WriteData(0x1B);
-  ili9341_WriteReg(LCD_DFC);
-  ili9341_WriteData(0x0A);
-  ili9341_WriteData(0xA2);
-  ili9341_WriteReg(LCD_POWER1);
-  ili9341_WriteData(0x10);
-  ili9341_WriteReg(LCD_POWER2);
-  ili9341_WriteData(0x10);
-  ili9341_WriteReg(LCD_VCOM1);
-  ili9341_WriteData(0x45);
-  ili9341_WriteData(0x15);
-  ili9341_WriteReg(LCD_VCOM2);
-  ili9341_WriteData(0x90);
-  ili9341_WriteReg(LCD_MAC);
-  ili9341_WriteData(0xC8);
-  ili9341_WriteReg(LCD_3GAMMA_EN);
-  ili9341_WriteData(0x00);
-  ili9341_WriteReg(LCD_RGB_INTERFACE);
-  ili9341_WriteData(0xC2);
-  ili9341_WriteReg(LCD_DFC);
-  ili9341_WriteData(0x0A);
-  ili9341_WriteData(0xA7);
-  ili9341_WriteData(0x27);
-  ili9341_WriteData(0x04);
+  LCD_IO_WriteReg(0xCA);
+  LCD_IO_WriteData(0xC3);
+  LCD_IO_WriteData(0x08);
+  LCD_IO_WriteData(0x50);
+  LCD_IO_WriteReg(LCD_POWERB);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteData(0xC1);
+  LCD_IO_WriteData(0x30);
+  LCD_IO_WriteReg(LCD_POWER_SEQ);
+  LCD_IO_WriteData(0x64);
+  LCD_IO_WriteData(0x03);
+  LCD_IO_WriteData(0x12);
+  LCD_IO_WriteData(0x81);
+  LCD_IO_WriteReg(LCD_DTCA);
+  LCD_IO_WriteData(0x85);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteData(0x78);
+  LCD_IO_WriteReg(LCD_POWERA);
+  LCD_IO_WriteData(0x39);
+  LCD_IO_WriteData(0x2C);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteData(0x34);
+  LCD_IO_WriteData(0x02);
+  LCD_IO_WriteReg(LCD_PRC);
+  LCD_IO_WriteData(0x20);
+  LCD_IO_WriteReg(LCD_DTCB);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteReg(LCD_FRMCTR1);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteData(0x1B);
+  LCD_IO_WriteReg(LCD_DFC);
+  LCD_IO_WriteData(0x0A);
+  LCD_IO_WriteData(0xA2);
+  LCD_IO_WriteReg(LCD_POWER1);
+  LCD_IO_WriteData(0x10);
+  LCD_IO_WriteReg(LCD_POWER2);
+  LCD_IO_WriteData(0x10);
+  LCD_IO_WriteReg(LCD_VCOM1);
+  LCD_IO_WriteData(0x45);
+  LCD_IO_WriteData(0x15);
+  LCD_IO_WriteReg(LCD_VCOM2);
+  LCD_IO_WriteData(0x90);
+  LCD_IO_WriteReg(LCD_MAC);
+  LCD_IO_WriteData(0xC8);
+  LCD_IO_WriteReg(LCD_3GAMMA_EN);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteReg(LCD_RGB_INTERFACE);
+  LCD_IO_WriteData(0xC2);
+  LCD_IO_WriteReg(LCD_DFC);
+  LCD_IO_WriteData(0x0A);
+  LCD_IO_WriteData(0xA7);
+  LCD_IO_WriteData(0x27);
+  LCD_IO_WriteData(0x04);
 
   /* Colomn address set */
-  ili9341_WriteReg(LCD_COLUMN_ADDR);
-  ili9341_WriteData(0x00);
-  ili9341_WriteData(0x00);
-  ili9341_WriteData(0x00);
-  ili9341_WriteData(0xEF);
+  LCD_IO_WriteReg(LCD_COLUMN_ADDR);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteData(0xEF);
   /* Page address set */
-  ili9341_WriteReg(LCD_PAGE_ADDR);
-  ili9341_WriteData(0x00);
-  ili9341_WriteData(0x00);
-  ili9341_WriteData(0x01);
-  ili9341_WriteData(0x3F);
-  ili9341_WriteReg(LCD_INTERFACE);
-  ili9341_WriteData(0x01);
-  ili9341_WriteData(0x00);
-  ili9341_WriteData(0x06);
+  LCD_IO_WriteReg(LCD_PAGE_ADDR);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteData(0x01);
+  LCD_IO_WriteData(0x3F);
+  LCD_IO_WriteReg(LCD_INTERFACE);
+  LCD_IO_WriteData(0x01);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteData(0x06);
 
-  ili9341_WriteReg(LCD_GRAM);
+  LCD_IO_WriteReg(LCD_GRAM);
   LCD_Delay(200);
 
-  ili9341_WriteReg(LCD_GAMMA);
-  ili9341_WriteData(0x01);
+  LCD_IO_WriteReg(LCD_GAMMA);
+  LCD_IO_WriteData(0x01);
 
-  ili9341_WriteReg(LCD_PGAMMA);
-  ili9341_WriteData(0x0F);
-  ili9341_WriteData(0x29);
-  ili9341_WriteData(0x24);
-  ili9341_WriteData(0x0C);
-  ili9341_WriteData(0x0E);
-  ili9341_WriteData(0x09);
-  ili9341_WriteData(0x4E);
-  ili9341_WriteData(0x78);
-  ili9341_WriteData(0x3C);
-  ili9341_WriteData(0x09);
-  ili9341_WriteData(0x13);
-  ili9341_WriteData(0x05);
-  ili9341_WriteData(0x17);
-  ili9341_WriteData(0x11);
-  ili9341_WriteData(0x00);
-  ili9341_WriteReg(LCD_NGAMMA);
-  ili9341_WriteData(0x00);
-  ili9341_WriteData(0x16);
-  ili9341_WriteData(0x1B);
-  ili9341_WriteData(0x04);
-  ili9341_WriteData(0x11);
-  ili9341_WriteData(0x07);
-  ili9341_WriteData(0x31);
-  ili9341_WriteData(0x33);
-  ili9341_WriteData(0x42);
-  ili9341_WriteData(0x05);
-  ili9341_WriteData(0x0C);
-  ili9341_WriteData(0x0A);
-  ili9341_WriteData(0x28);
-  ili9341_WriteData(0x2F);
-  ili9341_WriteData(0x0F);
+  LCD_IO_WriteReg(LCD_PGAMMA);
+  LCD_IO_WriteData(0x0F);
+  LCD_IO_WriteData(0x29);
+  LCD_IO_WriteData(0x24);
+  LCD_IO_WriteData(0x0C);
+  LCD_IO_WriteData(0x0E);
+  LCD_IO_WriteData(0x09);
+  LCD_IO_WriteData(0x4E);
+  LCD_IO_WriteData(0x78);
+  LCD_IO_WriteData(0x3C);
+  LCD_IO_WriteData(0x09);
+  LCD_IO_WriteData(0x13);
+  LCD_IO_WriteData(0x05);
+  LCD_IO_WriteData(0x17);
+  LCD_IO_WriteData(0x11);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteReg(LCD_NGAMMA);
+  LCD_IO_WriteData(0x00);
+  LCD_IO_WriteData(0x16);
+  LCD_IO_WriteData(0x1B);
+  LCD_IO_WriteData(0x04);
+  LCD_IO_WriteData(0x11);
+  LCD_IO_WriteData(0x07);
+  LCD_IO_WriteData(0x31);
+  LCD_IO_WriteData(0x33);
+  LCD_IO_WriteData(0x42);
+  LCD_IO_WriteData(0x05);
+  LCD_IO_WriteData(0x0C);
+  LCD_IO_WriteData(0x0A);
+  LCD_IO_WriteData(0x28);
+  LCD_IO_WriteData(0x2F);
+  LCD_IO_WriteData(0x0F);
 
-  ili9341_WriteReg(LCD_SLEEP_OUT);
+  LCD_IO_WriteReg(LCD_SLEEP_OUT);
   LCD_Delay(200);
-  ili9341_WriteReg(LCD_DISPLAY_ON);
+  LCD_IO_WriteReg(LCD_DISPLAY_ON);
 //  /* GRAM start writing */
-  ili9341_WriteReg(LCD_GRAM);
+  LCD_IO_WriteReg(LCD_GRAM);
 }
 
 void LCD_IO_WriteData(uint16_t RegValue)
@@ -322,7 +316,7 @@ void LCD_IO_WriteData(uint16_t RegValue)
 
   /* Reset LCD control line(/CS) and Send data */
   LCD_CS_LOW();
-  SPI_Tx(RegValue);
+  SPI_TxRx(RegValue, 0);
 
   /* Deselect: Chip Select high */
   LCD_CS_HIGH();
@@ -335,22 +329,12 @@ void LCD_IO_WriteReg(uint8_t Reg)
 
   /* Reset LCD control line(/CS) and Send command */
   LCD_CS_LOW();
-  SPI_Tx(Reg);
+  SPI_TxRx(Reg, 0);
 
   /* Deselect: Chip Select high */
   LCD_CS_HIGH();
 }
 
-
-void ili9341_WriteReg(uint8_t LCD_Reg)
-{
-  LCD_IO_WriteReg(LCD_Reg);
-}
-
-void ili9341_WriteData(uint16_t RegValue)
-{
-  LCD_IO_WriteData(RegValue);
-}
 
 void LCD_IO_Init(void)
 {
@@ -465,21 +449,25 @@ void LCD_GPIO_Config(void)
 
 }
 
-#define L3GD20_CTRL_REG1_ADDR         0x20  /* Control register 1 */
-#define L3GD20_CTRL_REG2_ADDR         0x21  /* Control register 2 */
-#define L3GD20_CTRL_REG3_ADDR         0x22  /* Control register 3 */
-#define L3GD20_CTRL_REG4_ADDR         0x23  /* Control register 4 */
-#define L3GD20_CTRL_REG5_ADDR         0x24  /* Control register 5 */
-
-void GYRO_IO_Write(uint8_t* pBuffer, uint8_t WriteAddr)
+void GYRO_IO_Write(uint16_t pBuffer, uint16_t WriteAddr)
 {
   GYRO_CS_LOW();
 
-  SPI_TxRx(WriteAddr);
-
-  SPI_TxRx(*pBuffer);
+  SPI_TxRx((WriteAddr<<8) | pBuffer, 0);
 
   GYRO_CS_HIGH();
+}
+
+uint16_t GYRO_IO_Read(uint8_t* pBuffer, uint8_t ReadAddr)
+{
+  uint16_t data=0;
+  GYRO_CS_LOW();
+
+  data = SPI_TxRx( (ReadAddr<<8) , 1 );
+
+  GYRO_CS_HIGH();
+
+  return data;
 }
 
 void Gyro_Init()
@@ -487,11 +475,12 @@ void Gyro_Init()
 	uint8_t ctrl = 0x00;
 	uint16_t initVals = 0x0000;
 
+	SPI5->CR1 |= SPI_CR1_DFF; //SPI in 16 bit mode
 
 	//(L3GD20_InitStructure.Power_Mode | L3GD20_InitStructure.Output_DataRate |
     //L3GD20_InitStructure.Axes_Enable | L3GD20_InitStructure.Band_Width
-	initVals = (uint16_t) (0x08 | 0x00 | 0x07 | 0x30);
-
+	//initVals = (uint16_t) (0x08 | 0x00 | 0x07 | 0x30);
+	initVals = 1<<3 |
 //	L3GD20_InitStructure.BlockData_Update | L3GD20_InitStructure.Endianness |
 //                        L3GD20_InitStructure.Full_Scale) << 8
 	initVals |= (uint16_t) ((0x00 | 0x00 | 0x10) << 8);
@@ -500,11 +489,13 @@ void Gyro_Init()
 
 	/* Write value to MEMS CTRL_REG1 register */
 	ctrl = (uint8_t) initVals;
-	GYRO_IO_Write(&ctrl, L3GD20_CTRL_REG1_ADDR);
+	GYRO_IO_Write(ctrl, L3GD20_CTRL_REG1_ADDR);
 
 	/* Write value to MEMS CTRL_REG4 register */
 	ctrl = (uint8_t) (initVals >> 8);
-	GYRO_IO_Write(&ctrl, L3GD20_CTRL_REG4_ADDR);
+	GYRO_IO_Write(ctrl, L3GD20_CTRL_REG4_ADDR);
+
+	GYRO_IO_Write(ctrl, L3GD20_CTRL_REG1_ADDR);
 }
 
 void Gyro_GPIO_Config()
@@ -512,4 +503,21 @@ void Gyro_GPIO_Config()
 	//CS - PC1
 	GPIOC->MODER   |= GPIO_MODER_MODE1_0;
 	GPIOC->OSPEEDR |= GPIO_OSPEEDR_OSPEED1_1;
+}
+void Gyro_ReadData()
+{
+//	//whoamI
+//	GYRO_IO_Write(0, (0x0f | 1<<7));
+	uint16_t x = 0;
+//	uint16_t y = 0;
+	uint8_t tmp = 0;
+
+	x = GYRO_IO_Read(&tmp, 0x28);
+	x = GYRO_IO_Read(&tmp, 0x29) << 8;
+
+//	y = GYRO_IO_Read(&tmp, 0x2A);
+//	y = GYRO_IO_Read(&tmp, 0x2B) << 8;
+
+
+	asm("nop");
 }
